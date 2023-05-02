c8a08408ce6d963264a1769b3dcc8a7fdf3819b6
DmytroKrylov <120196125+DmytroKrylov@users.noreply.github.com>
2023-05-03 02:20:55 +0300

    Second Task finish

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const ulElem = document.querySelector(".gallery");
+
+galleryItems.forEach((galleryItem) => {
+  const liElem = document.createElement("li");
+  liElem.classList.add("gallery__item");
+
+  const aElem = document.createElement("a");
+  aElem.classList.add("gallery__link");
+  aElem.setAttribute("href", galleryItem.original);
+
+  const imgElem = document.createElement("img");
+  imgElem.classList.add("gallery__image");
+  imgElem.setAttribute("src", galleryItem.preview);
+  imgElem.setAttribute("data-source", galleryItem.original);
+  imgElem.setAttribute("alt", galleryItem.description);
+
+  liElem.appendChild(aElem);
+  aElem.appendChild(imgElem);
+  ulElem.appendChild(liElem);
+});
+
+ulElem.addEventListener("click", (e) => {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  e.preventDefault();
+  console.log(e.target.getAttribute("data-source"));
+
+  const instance = basicLightbox.create(`
+    <img src='${e.target.getAttribute(
+      "data-source"
+    )}' width="800" height="600">`);
+  instance.show();
+
+  ulElem.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+});
